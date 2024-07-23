b823210435257b2351f39124ababa150fe36cf63
garce19 <garcejr@gmail.com>
2024-07-23 11:42:02 -0500

    fixing generators

--- lab_2/news_fetcher.py
+++ lab_2/news_fetcher.py
@@ -7,6 +7,8 @@ url = f"https://newsapi.org/v2/top-headlines?country=us&apiKey={API_KEY}"
 response = requests.get(url)
 data = response.json()
 
+print(data)
+
 articles = data['articles']
 meaningful_info = []
 
@@ -18,7 +20,7 @@ for article in articles:
     })
 
 # Guarda la información significativa en un archivo
-with open('./lab_2/news_data.txt', 'w', encoding='utf-8') as f:
+with open('./news_data.txt', 'w', encoding='utf-8') as f:
     for info in meaningful_info:
         f.write(f"Title: {info['title']}\n")
         f.write(f"Description: {info['description']}\n")
